ec749a5550d1d66dc852bd9474f4459e73d6b4bc
Miran Ismail <miim21@student.bth.se>
2025-09-04 12:41:15 +0200

    added structure

--- Uppgift2/getting_started/symbolTable.cc
+++ Uppgift2/getting_started/symbolTable.cc
@@ -1,3 +1,53 @@
 #include "stdlib.h"
 #include <iostream>
 #include "Node.h"
+
+
+using namespace std;
+
+class Record{
+public:
+  string name;
+  string type;
+
+  Record(const string& name,const string& type){
+    this.name= name;
+    this.type= type;
+  };
+  virtual ~Record() = default;
+
+  virtual void print(int indent= 0);
+
+};
+
+class VariableRecord: public Record{
+public:
+  string scope;
+  VariableRecord(const string& name, const string& type, const string& scope){
+    this.name = name;
+    this.type = type;
+    this.scope = scope;
+  }
+  void print(int indent) const override {
+  string  padding(indent,' ');
+  printf(padding<<"Variable"<<this.name);
+  }
+};
+
+class ClassRecord: public Record{
+
+
+};
+
+
+class MethodRecord: public Record{
+
+};
+
+
+class Scope{
+
+};
+class SymbolTable{
+
+};
