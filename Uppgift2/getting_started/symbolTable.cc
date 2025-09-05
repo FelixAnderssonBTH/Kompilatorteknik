c912396fe09e8468628a7937bbc83dd06b3e761e
Miran Ismail <miim21@student.bth.se>
2025-09-05 16:58:48 +0200

    data structure complete

--- Uppgift2/getting_started/symbolTable.cc
+++ Uppgift2/getting_started/symbolTable.cc
@@ -1,7 +1,7 @@
 #include "stdlib.h"
 #include <iostream>
-#include "Node.h"
 #include <map>
+#include <string>
 
 using namespace std;
 
@@ -11,12 +11,12 @@ public:
   string type;
 
   Record(const string& name,const string& type){
-    this.name= name;
-    this.type= type;
+    this->name= name;
+    this->type= type;
   };
   virtual ~Record() = default;
 
-  virtual void print(int indent= 0);
+  virtual void print(int indent= 0) const = 0;
 
 };
 
@@ -27,13 +27,13 @@ class VariableRecord: public Record{
 public:
   string scope;
   VariableRecord(const string& name, const string& type, const string& scope){
-    this.name = name;
-    this.type = type;
-    this.scope = scope;
+    this->name = name;
+    this->type = type;
+    this->scope = scope;
   }
   void print(int indent) const override {
   string  padding(indent,' ');
-  printf(padding<<"Variable"<<this.name);
+  cout << padding << "Variable: " << name << " : " << type << "\n";
   }
 };
 
@@ -44,20 +44,20 @@ public:
   map<string, VariableRecord*>parameters;
   map<string, VariableRecord*>local_variable;
 
-  MethodRecord(const string &name, const string &return_type)
-    : Record(name, "method"), return_type(return_type) {}
+  MethodRecord(const string &name, const string &return_type){
+    this->return_type=return_type;
+    this->name=name;
+  }
     
   void addParameters(VariableRecord *parms){
     parameters[parms->name] = parms;
   }
-
   void addLocalVariable(VariableRecord *var){
     local_variable[var->name] = var;
   }
 
   void print(int indent = 0) const override {
     string padding(indent, ' ');
-    string second_padding(indent+2, ' ');
 
     cout<< padding << "Method: " << name << " : " << return_type << "\n";
 
@@ -72,12 +72,98 @@ public:
 
 
 class ClassRecord: public Record{
+public:
+  map<string, VariableRecord*>fields;
+  map<string, MethodRecord*>methods;
+
+  ClassRecord(string& name){
+    this->name = name;
+  };
+  void addField(VariableRecord* var){
+    fields[var->name]= var;
+  }
+  void addMethod(MethodRecord* method){
+    methods[method->name]= method;
+  }
+  void print(int indent = 0) const override {
+    string padding(indent, ' ');
+    cout << padding << "Class: " << name << "\n";
+
+    for (auto &f : fields){
+      f.second->print(indent+2);
+    }
+    for (auto &m : methods){
+      m.second->print(indent+2);
+    }
+  }
 };
 
 
 class Scope{
+public:
+  string name;
+  Scope* parent;
+  map<string, Record*> symbols;
 
+  Scope(const string& name, Scope* parent = nullptr){
+    this->name = name;
+    this->parent = parent;
+  }
+  ~Scope(){
+    for (auto&s: symbols)
+      delete s.second;
+  }
+
+  void addRecord(Record* record){
+    symbols[record->name] = record;
+  }
+  Record* getRecord(const string& name){
+    if(symbols.find(name) != symbols.end())
+      return symbols[name];
+    if (parent)
+      return parent->getRecord(name);
+    return nullptr;
+  }
+  void print(int indent = 0) const{
+    string padding(indent, ' ');
+    cout << padding << "Scope: " << name << "\n";
+    for (auto& s : symbols){
+      s.second->print(indent+2);
+    }
+  }
 };
 class SymbolTable{
+public:
+  Scope* globalScope;
+  Scope* currentScope;
 
+  SymbolTable(){
+    globalScope = new Scope("global");
+    currentScope = globalScope;
+  }
+  ~SymbolTable(){
+    delete globalScope;
+  }
+  void enterScope(const string& name){
+    Scope* newScope = new Scope(name,currentScope);
+    currentScope = newScope;
+  }
+  void exitScope(){
+    if(currentScope->parent)
+      currentScope = currentScope->parent;
+  }
+  void addRecord(Record* record){
+    currentScope->addRecord(record);
+  }
+  Record* getRecord(const string& name){
+    return currentScope->getRecord(name);
+  }
+  void reset(){
+    delete globalScope;
+    globalScope = new Scope("global");
+    currentScope = globalScope;
+  }
+  void print() const {
+    globalScope->print();
+  }
 };
